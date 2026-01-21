aca30b5e1a1271e2c622fce07ff0ed278d5b3a70
정연 <1023graces@gmail.com>
2026-01-21 21:34:13 +0900

    ✨ feat: 데이터 모델(User, Post, Comment) 및 Enum 구현
    - User, Post, Comment 등 주요 엔티티 클래스 생성
    - 게시글 상태/유형 관리를 위한 Enum 클래스 정의

---
+++ app/src/main/java/com/team/jubjub/data/model/enum/NotificationType.kt
@@ -0,0 +1,6 @@
+package com.team.jubjub.data.model.enum
+
+enum class NotificationType {
+    COMMENT, // 댓글 알림
+    KEYWORD  // 키워드 알림
+}
---
+++ app/src/main/java/com/team/jubjub/data/model/enum/PostCategory.kt
@@ -0,0 +1,9 @@
+package com.team.jubjub.data.model.enum
+
+enum class PostCategory {
+    CLOTHING, // 의류
+    FOOD, // 식품
+    ELECTRONICS, // 전자기기
+    BOOK, // 도서
+    OTHERS // 기타
+}
