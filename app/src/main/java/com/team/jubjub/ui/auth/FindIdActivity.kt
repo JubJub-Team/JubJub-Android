73e8660110fe8337ed0ca1f861b20ec249d41e10
jungyun <wjddbs7360@naver.com>
2026-01-25 08:55:14 +0900

    ✨ feat: 아이디/비밀번호 찾기 기능 구현
    - 아이디 및 비밀번호 찾기 화면 구현
    - 관련 화면 이동 및 기본 흐름 구성

--- app/src/main/java/com/team/jubjub/ui/auth/FindIdActivity.kt
+++ app/src/main/java/com/team/jubjub/ui/auth/FindIdActivity.kt
@@ -1,8 +1,9 @@
-package com.team.jubjub
+package com.team.jubjub.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.team.jubjub.ui.auth.FindPwActivity
 import com.team.jubjub.databinding.ActivityFindIdBinding
 
 class FindIdActivity : AppCompatActivity() {
