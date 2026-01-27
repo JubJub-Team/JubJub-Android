eeedbb967ebcd56f66508876758a1e0232d5dd51
minju <sw.deliminju@gmail.com>
2026-01-27 17:45:49 +0900

    🎨 design: 커스텀 다이얼로그 디자인 개선 및 자동 텍스트 크기 조절 적용 (#34)

--- app/src/main/java/com/team/jubjub/ui/write/LocationPickerActivity.kt
+++ app/src/main/java/com/team/jubjub/ui/write/LocationPickerActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.app.Activity
 import android.content.Intent
 import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
